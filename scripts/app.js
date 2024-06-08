45eb32b8c8aa2bee5c960024377f940dd5bb5026
Vyacheslav <korzan.va@mail.ru>
2024-06-08 23:02:45 +0600

    2024-06-08 continue what is

---
+++ scripts/app.js
@@ -0,0 +1,47 @@
+const getId = (id) => document.getElementById(id);
+
+
+const
+  $WhatIsBtnTable                  = getId("what-is-btn-table"),
+  $WhatIsBtnDashboard              = getId("what-is-btn-dashboard"),
+  $WhatIsBtnTableIconActive        = getId("what-is-btn-table-icon-active"),
+  $WhatIsBtnTableIconNotActive     = getId("what-is-btn-table-icon-not-active"),
+  $WhatIsBtnDashbordIconActive     = getId("what-is-btn-dashboard-icon-active"),
+  $WhatIsBtnDashbordIconNotActive  = getId("what-is-btn-dashboard-icon-not-active"),
+  $WhatIsTopRowBox                 = getId("what-is-top-row-box"),
+  $WhatIsTopRowBoxTable            = getId("what-is-top-row-box-table"),
+  $WhatIsTopRowBoxDashboard        = getId("what-is-top-row-box-dashboard");
+
+
+$WhatIsBtnTable.addEventListener("click", () => {
+  if ($WhatIsBtnTable.classList.contains("active")) return;
+
+  $WhatIsBtnDashboard.classList.remove("active");
+  $WhatIsBtnTableIconActive.classList.remove("not-visible");
+  $WhatIsBtnDashbordIconActive.classList.remove("not-visible");
+
+  $WhatIsBtnTable.classList.add("active");
+  $WhatIsBtnTableIconNotActive.classList.add("not-visible");
+  $WhatIsBtnDashbordIconNotActive.classList.add("not-visible");
+
+  $WhatIsTopRowBox.classList.remove("ml-210"); // Unshift image from right
+  $WhatIsTopRowBoxTable.classList.remove("not-visible");
+  $WhatIsTopRowBoxDashboard.classList.add("not-visible");
+});
+
+
+$WhatIsBtnDashboard.addEventListener("click", () => {
+  if ($WhatIsBtnDashboard.classList.contains("active")) return;
+
+  $WhatIsBtnTable.classList.remove("active");
+  $WhatIsBtnTableIconNotActive.classList.remove("not-visible");
+  $WhatIsBtnDashbordIconNotActive.classList.remove("not-visible");
+
+  $WhatIsBtnDashboard.classList.add("active");
+  $WhatIsBtnTableIconActive.classList.add("not-visible");
+  $WhatIsBtnDashbordIconActive.classList.add("not-visible");
+
+  $WhatIsTopRowBox.classList.add("ml-210"); // Shift image to the right
+  $WhatIsTopRowBoxTable.classList.add("not-visible");
+  $WhatIsTopRowBoxDashboard.classList.remove("not-visible");
+});
