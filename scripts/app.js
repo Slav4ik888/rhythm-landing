22afeda4e914125cc2db5bef98b3c0135fc92bac
Vyacheslav <korzan.va@mail.ru>
2024-06-16 21:36:04 +0600

    2024-06-16 continue footer

--- scripts/app.js
+++ scripts/app.js
@@ -99,8 +99,37 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       .querySelector(anchor.getAttribute('href'))
       .scrollIntoView({
         behavior: 'smooth',
-        block: 'start'
+        // block: 'start'
       });
+  });
+});
+
+
+// -------------------------
+// --  ПЛАВНАЯ ПРОКРУТКА С ОТСТУПОМ СВЕРХУ
+// ----------------------------------------------------------------------------------
+
+// Устанавливаем значение отступа сверху
+let topSpace = -100;
+
+// Получаем все элементы с классом internal-link
+const links = document.querySelectorAll('.internal-link');
+
+// Добавляем обработчик события клика
+links.forEach(link => {
+  link.addEventListener('click', function (event) {
+
+    // Получаем элемент, на который указывает ссылка
+    const target = document.getElementById(this.getAttribute('href')?.slice(1));
+
+    // Вычисляем позицию элемента относительно верха окна
+    let top = target.offsetTop;
+
+    // Добавляем отступ
+    top += topSpace; // Пример значения отступа
+
+    // Прокручиваем страницу до нужной позиции
+    window.scrollTo({ top, behavior: 'smooth' });
   });
 });
 
