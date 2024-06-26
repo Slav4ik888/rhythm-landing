3efa1b6fcb08e787722dca5e41f05ced43dc887a
Vyacheslav <korzan.va@mail.ru>
2024-06-26 18:22:30 +0600

    2024-06-26 add yandex metrika

--- server-src/index.ts
+++ server-src/index.ts
@@ -3,7 +3,7 @@ import app from './app';
 app.listen(process.env.PORT || 9090, () => console.log(`Listening on port ${process.env.PORT || 9090}!`))
 
 // 
-// git add . && git commit -m "2024-06-25 add mail prices" && git push origin main
+// git add . && git commit -m "2024-06-26 add yandex metrika" && git push origin main
 // 
 
 // 200 OK
