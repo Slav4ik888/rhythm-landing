7eaf3704cc5054ed00e1779573c1f8e37e1d5c33
Vyacheslav <korzan.va@mail.ru>
2024-06-27 22:46:19 +0600

    2024-06-27 start adapt desing header & main

---
+++ scripts/sections/header.js
@@ -0,0 +1,38 @@
+import { getById } from '../utils/get-by-id.js';
+
+const
+  whatIs = getById('what-is'),
+  advantages = getById('advantages'),
+  videoPresentation = getById('video-presentation'),
+  usefullForBusiness = getById('usefull-for-business'),
+  examples = getById('examples'),
+  getDemo = getById('get-demo'),
+  primarySecondary = getById('primary-secondary'),
+  devTimeline = getById('dev-timeline'),
+  whyWe = getById('why-we'),
+  prices = getById('prices'),
+  footer = getById('footer');
+
+whatIs.classList.add('not-visible');
+advantages.classList.add('not-visible');
+videoPresentation.classList.add('not-visible');
+usefullForBusiness.classList.add('not-visible');
+examples.classList.add('not-visible');
+getDemo.classList.add('not-visible');
+primarySecondary.classList.add('not-visible');
+devTimeline.classList.add('not-visible');
+whyWe.classList.add('not-visible');
+prices.classList.add('not-visible');
+footer.classList.add('not-visible');
+
+
+// const
+//   $menuBtn = getById('.menu__button'),
+//   $menuList = getById('.menu__list');
+
+// $menuBtn.addEventListener('click', () => { 
+//   let expanded = $menuBtn.getAttribute('aria-expanded') === 'true' || 'false';
+//   $menuBtn.setAttribute('aria-expanded', ! expanded);
+//   $menuBtn.classList.toggle('menu__button--open');
+//   $menuList.classList.toggle('menu__list--open');
+// });
--- scripts/sections/index.js
+++ scripts/sections/index.js
@@ -1,5 +1,6 @@
 import './dev-timeline.js';
 import './get-demo.js';
+import './header.js';
 import './prices.js';
 import './video-presentation.js';
 import './what-is.js';
