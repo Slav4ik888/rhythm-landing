1d44a0981fdae55bc05c38bcffd6fdda82a6f957
Vyacheslav <korzan.va@mail.ru>
2024-08-05 22:04:10 +0300

    2024-08-05 fix settings

--- scripts/consts/index.js
+++ scripts/consts/index.js
@@ -1,6 +1,6 @@
 
 export const SETTINGS = {
-  mode   : 'dev', // 'prod', //  
+  mode   : 'prod', // 'dev', //   
   prefix : '/api',
   get url() {
     return (this.mode === 'dev' ? 'http://localhost:9090' : 'https://rhy.thm.su') + this.prefix;
